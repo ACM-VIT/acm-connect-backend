c69d4196ca06ecf8bdf2a62ceca6a35289e6c284
ANISH0309 <mittalanish789@gmail.com>
2021-07-29 12:35:10 +0530

    feat: Add linters
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const compression = require('compression')
+const express = require("express");
+const bodyParser = require("body-parser");
+const compression = require("compression");
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(compression())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(compression());
 
-const port = process.env.PORT || 3001
-console.log(process.env.DATABASEURL)
+const port = process.env.PORT || 3001;
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.DATABASEURL
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.DATABASEURL,
 });
 
-let db = admin.firestore();
-let a = db.collection('groups')
+const database = admin.firestore();
+const groups = database.collection("groups");
 
-app.post('/data', async (req, res) => {
-    let docRef = a.doc(req.body.name)
-    await docRef.set({
-        name: req.body.name,
-        joiningLink: req.body.joiningLink,
-        maxLimit: req.body.maxLimit,
-        currentCount: req.body.currentCount,
-        allow_more: req.body.allow_more
-    });
-    res.send('done');
-})
+app.post("/data", async (req, res) => {
+  const docRef = groups.doc(req.body.name);
+  await docRef.set({
+    name: req.body.name,
+    joiningLink: req.body.joiningLink,
+    maxLimit: req.body.maxLimit,
+    currentCount: req.body.currentCount,
+    allow_more: req.body.allow_more,
+  });
+  res.json({ Message: "Action Completed" });
+});
 
-app.post('/update', async (req, res) => {
-    let docRef = a.doc(req.body.name)
-    await docRef.update({
-        currentCount: req.body.currentCount
-    });
-    res.send('done');
-})
+app.post("/update", async (req, res) => {
+  const docRef = groups.doc(req.body.name);
+  await docRef.update({
+    currentCount: req.body.currentCount,
+  });
+  res.json({ Message: "Action Completed" });
+});
 
-app.get('/display', async (req, res) => {
-    const snapshot = await a.get();
-    snapshot.forEach(doc => {
-        console.log(doc.id, '=>', doc.data());
-    });
-})
+app.get("/display", async (req, res) => {
+  const groupData = await groups.get();
+  const data = [];
+  groupData.forEach((doc) => {
+    const obj = doc.data();
+    obj.id = doc.id;
+    data.push(obj);
+  });
+  res.json({ arr: data });
+});
 
-app.listen(port, (req, res) => {
-    console.info(`Running on ${port}`)
-})
+app.listen(port, () => {
+  console.info(`Running on ${port}`);
+});
