946affa961a257849e5cde851e742f01bfd816d5
Swarup Kharul <swarupkharulsk@gmail.com>
2021-08-15 00:04:20 +0530

    added env

--- src/services/memory.js
+++ src/services/memory.js
@@ -4,39 +4,39 @@ let lastUpdated = 0;
 
 /** a getter to get the memory */
 function getMemory() {
-    return memory;
+  return memory;
 }
 
 /** a setter to set a particular memory value */
 function setMemory(key, value) {
-    memory[key] = value;
+  memory[key] = value;
 }
 
 /** a bulk setter to save an array of memory vallllues */
 function setMemoryArray(memoryArray) {
-    memory = memoryArray;
+  memory = memoryArray;
 }
 
 /** trigger to set lastUpdates to now */
 function triggerLastUpdated() {
-    lastUpdated = Date.now();
+  lastUpdated = Date.now();
 }
 
 /** check if memory is empty */
 function isMemoryEmpty() {
-    return memory === undefined || memory.length === 0;
+  return memory === undefined || memory.length === 0;
 }
 
 /** cllear the memory */
 function clearMemory() {
-    memory = new Array();
+  memory = new Array();
 }
 
 module.exports = {
-    getMemory,
-    setMemory,
-    setMemoryArray,
-    clearMemory,
-    triggerLastUpdated,
-    isMemoryEmpty,
+  getMemory,
+  setMemory,
+  setMemoryArray,
+  clearMemory,
+  triggerLastUpdated,
+  isMemoryEmpty,
 };
